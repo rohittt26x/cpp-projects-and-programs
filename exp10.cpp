2417e425ea53bd774e8930e7e9f7c36fe49c4840
Rohit J. Pokale <rohitjpokale@gmail.com>
2026-04-20 12:35:38 +0530

    Create exp10.cpp

---
+++ exp10.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+class Shape {
+public:
+    virtual void area() = 0;
+};
+
+class Circle : public Shape {
+    float r;
+public:
+    Circle(float x) { r = x; }
+
+    void area() {
+        cout << "Area of Circle: " << 3.14 * r * r << endl;
+    }
+};
+
+int main() {
+    Circle c(5);
+    c.area();
+    return 0;
+}
